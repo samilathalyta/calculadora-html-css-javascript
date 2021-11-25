e731f1045619ae9b8a8eb19d3f58d7660aa861cb
Thalyta Campos <samila.thalytaa@gmail.com>
2021-11-24 22:26:28 -0300

    Calculadora versao 1

---
+++ script.js
@@ -0,0 +1,36 @@
+
+ 
+
+function insert(num){
+
+   
+    var resultado = document.querySelector('.resultado').innerHTML 
+    
+    
+    var resultado = document.querySelector('.resultado').innerHTML = resultado + num
+
+       
+}
+    
+
+
+function apagar (){
+    var resultado = document.querySelector('.resultado').innerHTML
+     document.querySelector('.resultado').innerHTML = resultado.substring(0, resultado.length -1)
+
+}
+
+function clean() {
+    document.querySelector('.resultado').innerHTML = " "
+}
+
+function calc(){
+    var resultado = document.querySelector('.resultado').innerHTML
+    if(resultado){
+        document.querySelector('.resultado').innerHTML = eval(resultado)
+    }
+
+}
+
+
+
